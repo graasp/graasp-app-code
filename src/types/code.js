fac1dee6b36192154514c5055a4a79240e495176
Takayuki NAGAI <nagai@kit.ac.jp>
2019-09-01 10:08:22 -0700

    feat: ace editor support in settings

--- src/types/code.js
+++ src/types/code.js
@@ -1,4 +1,6 @@
 export const SET_CODE = 'SET_CODE';
+export const SET_HEADER_CODE = 'SET_HEADER_CODE';
+export const SET_FOOTER_CODE = 'SET_FOOTER_CODE';
 export const RUN_CODE_SUCCEEDED = 'RUN_CODE_SUCCEEDED';
 export const RUN_CODE_FAILED = 'RUN_CODE_FAILED';
 export const PRINT_OUTPUT = 'PRINT_OUTPUT';
