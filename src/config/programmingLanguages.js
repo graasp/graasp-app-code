ef801bf78c7f956c7768cc782b34122f78f47a80
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2020-01-20 18:45:59 +0100

    chore: change default language to python

--- src/config/programmingLanguages.js
+++ src/config/programmingLanguages.js
@@ -1,3 +1,3 @@
 export const JAVASCRIPT = 'javascript';
 export const PYTHON = 'python';
-export const DEFAULT_PROGRAMMING_LANGUAGE = JAVASCRIPT;
+export const DEFAULT_PROGRAMMING_LANGUAGE = PYTHON;
