52f96b26d17936e1bc8574eca36c19dc41dfa002
Kim <pyphilia@gmail.com>
2020-03-11 12:59:09 +0100

    feat: add export as csv button
    close #39

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -13,6 +13,7 @@ import {
 } from '@material-ui/icons';
 import { withTranslation } from 'react-i18next';
 import { addQueryParamsToUrl } from '../../utils/url';
+import DownloadCSVButton from '../modes/teacher/DownloadCSVButton';
 import { ReactComponent as Logo } from '../../resources/logo.svg';
 import {
   DEFAULT_MODE,
@@ -198,6 +199,7 @@ class Header extends Component {
       <IconButton onClick={this.handleRefresh} key="refresh">
         <RefreshIcon color="secondary" />
       </IconButton>,
+      <DownloadCSVButton />,
     ];
 
     if (view === DEFAULT_VIEW) {
---
+++ src/components/modes/teacher/DownloadCSVButton.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CSVLink } from 'react-csv';
+import { connect } from 'react-redux';
+import DownloadIcon from '@material-ui/icons/CloudDownload';
+import { withTranslation } from 'react-i18next';
+
+const DownloadCSVButton = ({ appInstanceResources, users, t }) => {
+  if (!appInstanceResources.length || !users.length) {
+    return null;
+  }
+
+  const csvData = appInstanceResources.map(({ data, user }) => {
+    const userData = users.find(({ id }) => id === user);
+    const name = userData ? userData.name : t('Anonymous');
+    return { name, data };
+  });
+
+  return (
+    <CSVLink data={csvData}>
+      <DownloadIcon color="secondary" />
+    </CSVLink>
+  );
+};
+
+DownloadCSVButton.propTypes = {
+  t: PropTypes.func.isRequired,
+  appInstanceResources: PropTypes.arrayOf(
+    PropTypes.shape({
+      data: PropTypes.string.isRequired,
+      user: PropTypes.string.isRequired,
+    })
+  ),
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+DownloadCSVButton.defaultProps = {
+  users: [],
+  appInstanceResources: [],
+};
+
+const mapStateToProps = ({ appInstanceResources, users }) => ({
+  appInstanceResources: appInstanceResources.content,
+  users: users.content,
+});
+
+const ConnectedComponent = connect(
+  mapStateToProps,
+  null
+)(DownloadCSVButton);
+
+export default withTranslation()(ConnectedComponent);
